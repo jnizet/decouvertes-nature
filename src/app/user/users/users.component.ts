e77579c13a174b9a935d77e61ce2ff98ebae70b5
jnizet <jb@ninja-squad.com>
2022-02-13 18:52:27 +0100

    feat: reset password email link on users

--- src/app/user/users/users.component.ts
+++ src/app/user/users/users.component.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AdministeredUser, UserService } from '../user.service';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, from, Observable, switchMap, tap, timer } from 'rxjs';
 import {
+  envelope,
   pencilSquare,
   plusCircle,
   shieldFill,
@@ -20,10 +21,30 @@ export class UsersComponent {
     admin: shieldFill,
     disabled: xOctagonFill,
     edit: pencilSquare,
-    addUser: plusCircle
+    addUser: plusCircle,
+    resetPasswordEmail: envelope
   };
+
+  copied = new BehaviorSubject(false);
 
   constructor(userService: UserService) {
     this.users$ = userService.listUsers();
+  }
+
+  copyEmail(user: AdministeredUser) {
+    const path = window.location.origin + '/reset-password?email=' + encodeURIComponent(user.email);
+    const email = `Bonjour ${user.displayName}.
+
+Pour pouvoir créer de nouvelles activités dans l'application "Découvertes Nature",
+il te faudra choisir un mot de passe en te rendant à l'adresse suivante:
+${path}.
+`;
+    from(navigator.clipboard.writeText(email))
+      .pipe(
+        tap(() => this.copied.next(true)),
+        switchMap(() => timer(3000)),
+        tap(() => this.copied.next(false))
+      )
+      .subscribe();
   }
 }
