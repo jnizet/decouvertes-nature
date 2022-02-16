5af0ee7425c695b86fcbd06b1c1f1a8ff8b9f406
jnizet <jb@ninja-squad.com>
2022-02-16 08:48:54 +0100

    test: test against other browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -48,20 +48,19 @@ const config: PlaywrightTestConfig = {
       use: {
         ...devices['Desktop Chrome']
       }
+    },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox']
+      }
+    },
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari']
+      }
     }
-
-    // {
-    //   name: 'firefox',
-    //   use: {
-    //     ...devices['Desktop Firefox']
-    //   }
-    // },
-    // {
-    //  name: 'webkit',
-    //  use: {
-    //    ...devices['Desktop Safari']
-    //  }
-    // }
 
     /* Test against mobile viewports. */
     // {
