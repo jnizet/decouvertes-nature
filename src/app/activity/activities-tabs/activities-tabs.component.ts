8da1c178592c8ef0a0c59d61c5569a14f9b89122
jnizet <jb@ninja-squad.com>
2022-02-26 10:07:48 +0100

    chore: make activities tabs OnPush

--- src/app/activity/activities-tabs/activities-tabs.component.ts
+++ src/app/activity/activities-tabs/activities-tabs.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { plusCircle } from '../../bootstrap-icons/bootstrap-icons';
 import { IsActiveMatchOptions } from '@angular/router';
 
 @Component({
   selector: 'dn-activities-tabs',
   templateUrl: './activities-tabs.component.html',
-  styleUrls: ['./activities-tabs.component.scss']
+  styleUrls: ['./activities-tabs.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ActivitiesTabsComponent {
   icons = {
