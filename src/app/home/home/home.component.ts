12aba5692a9a1c2897f691a1eb8eec43c0255523
jnizet <jb@ninja-squad.com>
2022-02-26 09:56:03 +0100

    chore: make home OnPush

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { interval, map, Observable } from 'rxjs';
 import { Auth, authState, User } from '@angular/fire/auth';
 import { boxArrowInRight, calendar2Event, cardList } from '../../bootstrap-icons/bootstrap-icons';
 
 @Component({
   selector: 'dn-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
   vm$: Observable<{ user: User | null }>;
@@ -15,6 +16,10 @@ export class HomeComponent {
     activities: cardList,
     calendar: calendar2Event
   };
+
+  rollingIcon$ = interval(1000).pipe(
+    map(i => (i % 2 === 0 ? this.icons.login : this.icons.calendar))
+  );
 
   constructor(auth: Auth) {
     this.vm$ = authState(auth).pipe(map(user => ({ user })));
