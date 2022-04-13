45eb12283318ff12ef94a104910310cce078cff2
jnizet <jb@ninja-squad.com>
2022-04-13 14:03:31 +0200

    chore: make change password form typed

--- src/app/change-password/change-password/change-password.component.ts
+++ src/app/change-password/change-password/change-password.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import {
   Auth,
   EmailAuthProvider,
@@ -10,42 +10,34 @@ import {
 import { from, switchMap } from 'rxjs';
 import { Router } from '@angular/router';
 
-interface FormValue {
-  currentPassword: string;
-  newPassword: string;
-}
-
 @Component({
   selector: 'dn-change-password',
   templateUrl: './change-password.component.html',
   styleUrls: ['./change-password.component.scss']
 })
 export class ChangePasswordComponent {
-  form: UntypedFormGroup;
+  form = new FormGroup({
+    currentPassword: new FormControl('', [Validators.required]),
+    newPassword: new FormControl('', [Validators.required, Validators.minLength(8)])
+  });
   error = false;
 
-  constructor(private auth: Auth, private router: Router) {
-    const config: Record<keyof FormValue, any> = {
-      currentPassword: new UntypedFormControl('', [Validators.required]),
-      newPassword: new UntypedFormControl('', [Validators.required, Validators.minLength(8)])
-    };
-    this.form = new UntypedFormGroup(config);
-  }
+  constructor(private auth: Auth, private router: Router) {}
 
   changePassword() {
     if (this.form.invalid) {
       return;
     }
 
-    const formValue: FormValue = this.form.value;
+    const formValue = this.form.value;
     const user: User = this.auth.currentUser!;
     from(
       reauthenticateWithCredential(
         user,
-        EmailAuthProvider.credential(user.email!, formValue.currentPassword)
+        EmailAuthProvider.credential(user.email!, formValue.currentPassword!)
       )
     )
-      .pipe(switchMap(() => from(updatePassword(user, formValue.newPassword))))
+      .pipe(switchMap(() => from(updatePassword(user, formValue.newPassword!))))
       .subscribe({
         next: () => this.router.navigate(['/']),
         error: () => (this.error = true)
