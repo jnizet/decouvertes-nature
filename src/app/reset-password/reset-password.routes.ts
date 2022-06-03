e8e92921ea8338b384bace9734a01efb420806a7
jnizet <jb@ninja-squad.com>
2022-06-03 08:52:45 +0200

    refactor: make email action and reset password components standalone

--- src/app/reset-password/reset-password.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ResetPasswordComponent } from './reset-password/reset-password.component';
-
-export const RESET_PASSWORD_ROUTES: Routes = [
-  {
-    path: '',
-    component: ResetPasswordComponent
-  }
-];
