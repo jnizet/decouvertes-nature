e556c4eb8b661078c6b04d3029db6ba720bbc4db
jnizet <jb@ninja-squad.com>
2022-06-03 09:27:36 +0200

    refactor: make activity card standalone

--- src/app/activity/activity-card/activity-card.component.ts
+++ src/app/activity/activity-card/activity-card.component.ts
@@ -6,12 +6,19 @@ import {
   emojiLaughing,
   pinMap
 } from '../../bootstrap-icons/bootstrap-icons';
+import { CommonModule } from '@angular/common';
+import { IconDirective } from '../../icon/icon.directive';
+import { ActivityDatePipe } from '../../activity-date-pipe/activity-date.pipe';
+import { ActivityTypePipe } from '../../activity-type-pipe/activity-type.pipe';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'dn-activity-card',
   templateUrl: './activity-card.component.html',
   styleUrls: ['./activity-card.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [CommonModule, RouterModule, IconDirective, ActivityTypePipe, ActivityDatePipe]
 })
 export class ActivityCardComponent {
   @Input() activity!: Activity;
