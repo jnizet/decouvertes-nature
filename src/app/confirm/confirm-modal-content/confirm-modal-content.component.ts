ae892d0249613550793f330bcdc26cf0e655db1a
jnizet <jb@ninja-squad.com>
2022-06-03 08:43:43 +0200

    refactor: make confirm component standalone

--- src/app/confirm/confirm-modal-content/confirm-modal-content.component.ts
+++ src/app/confirm/confirm-modal-content/confirm-modal-content.component.ts
@@ -1,11 +1,14 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'dn-confirm-modal-content',
   templateUrl: './confirm-modal-content.component.html',
   styleUrls: ['./confirm-modal-content.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [CommonModule]
 })
 export class ConfirmModalContentComponent {
   @Input() message?: string;
