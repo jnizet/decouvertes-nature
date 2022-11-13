67e32b41579035de74e09c4c92b6f2aa7a15a7e7
jnizet <jb@ninja-squad.com>
2022-11-13 11:07:19 +0100

    feat: store year in local storage

--- src/app/year.service.ts
+++ src/app/year.service.ts
@@ -1,15 +1,34 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
+function storeYear(year: number) {
+  window.localStorage.setItem('year', `${year}`);
+}
+
+function readYear(): number | null {
+  const readYear = window.localStorage.getItem('year');
+  if (readYear) {
+    const parsedReadYear = parseInt(readYear);
+    if (isNaN(parsedReadYear)) {
+      return null;
+    } else {
+      return parsedReadYear;
+    }
+  } else {
+    return null;
+  }
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class YearService {
-  private yearSubject = new BehaviorSubject<number>(new Date().getFullYear());
+  private yearSubject = new BehaviorSubject<number>(readYear() ?? new Date().getFullYear());
 
   readonly year$: Observable<number> = this.yearSubject.asObservable();
 
   set(year: number) {
     this.yearSubject.next(year);
+    storeYear(year);
   }
 }
