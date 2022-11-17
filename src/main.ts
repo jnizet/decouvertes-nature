909f8fbc1d729c0bdced6eb1e3375c0ec0bc84b4
jnizet <jb@ninja-squad.com>
2022-11-17 11:26:23 +0100

    chore: change way of handling polyfills

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,9 @@
-import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { APP_ROUTES } from './app/app.routes';
 import '@angular/common/locales/global/fr';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 bootstrapApplication(AppComponent, {
   providers: [...environment.firebaseProviders, provideRouter(APP_ROUTES)]
