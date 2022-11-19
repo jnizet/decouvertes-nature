3bde1805fa82ef08ff19d9b24b63e02b7b832feb
jnizet <jb@ninja-squad.com>
2022-11-19 15:30:33 +0100

    test: add test for map

--- e2e/utils.ts
+++ e2e/utils.ts
@@ -1,6 +1,15 @@
 import { expect, Page } from '@playwright/test';
 import * as crypto from 'crypto';
 import AxeBuilder from '@axe-core/playwright';
+import { initializeApp } from 'firebase-admin/app';
+import { CollectionReference, getFirestore } from 'firebase-admin/firestore';
+
+process.env['FIRESTORE_EMULATOR_HOST'] = 'localhost:7070';
+initializeApp({ projectId: 'decouvertes-nature' });
+
+interface Activity {
+  title: string;
+}
 
 export async function login(page: Page, email = 'jnizet@gmail.com') {
   await page.goto('/');
@@ -27,4 +36,16 @@ export async function checkAccessibility(page: Page, message?: string) {
     .disableRules('aria-allowed-attr')
     .analyze();
   expect(accessibilityScanResults.violations, message).toEqual([]);
+}
+
+export async function deleteAllActivitiesExceptRomimine() {
+  const activitiesCollection = getFirestore().collection('activities') as CollectionReference<Activity>;
+  const documents = await activitiesCollection.listDocuments();
+  for (let i = 0; i < documents.length; i++) {
+    const document = await documents[i].get();
+    const activity = document.data();
+    if (activity && activity.title !== 'Romimine') {
+      await documents[i].delete();
+    }
+  }
 }
